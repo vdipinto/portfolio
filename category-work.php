bec919d980007c800f27580046857aa8ebf27605
Vito Dipinto <vitodipinto@outlook.com>
2019-01-16 19:29:36 +0000

    created work category

---
+++ category-work.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * The template file for the category-work.php
+ *
+ * This is the template for the category page with slug mainwebdesign
+ * It is used to display the category page for the MA in Web Design Category. Remind you that the Template Hierachy for Category pages is as follow:
+ * 
+ * 1.category-slug.php
+ * 2.category-ID.php
+ * 3.category.ph
+ * 4.archive.php
+ * 5.index.php
+ * 
+ *
+ *
+ * @link https://codex.wordpress.org/Category_Templates
+ *
+ * @package portfolio
+ */
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		// Check if there are any posts to display
+		if ( have_posts() ) : ?>
+ 
+		<header class="category-header">
+			<h1 class="category-title"><?php single_cat_title( '', true ); ?></h1>
+		
+		
+		<?php
+		// Display optional category description
+		if ( category_description() ) : ?>
+		    <div class="archive-meta"><?php echo category_description(); ?></div>
+		<?php endif; ?>
+		</header>
+		
+		<div class="work-posts">
+            <?php
+            
+            // The Loop
+            
+            while ( have_posts() ) : the_post();
+
+                get_template_part( 'template-parts/content-work', get_post_type() );
+            
+            endwhile; 
+            
+        else: ?>
+            <p>Sorry, no posts matched your criteria.</p>
+            
+            
+        <?php endif; ?>
+	    </div><!-- .work-posts -->
+    </div>
+    
+<?php get_footer(); ?>
---
+++ template-parts/content-work.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template part for displaying work example posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package portfolio
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('card'); ?>>
+    <?php portfolio_post_thumbnail('category-thumb'); ?>
+	<header class="entry-header">
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;?>
+	</header><!-- .entry-header -->
+    <p><?php echo get_post_meta($post->ID, 'description', true); ?></p>
+    <footer class="entry-footer">
+        <a href="<?php esc_url( the_permalink() ) ?>" class="btn">See more</a>
+	</footer><!-- .entry-footer -->
+
+    
+    
+    
+
+</article><!-- #post-<?php the_ID(); ?> -->
