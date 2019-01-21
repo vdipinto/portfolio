2a40df8d5d64af244ecfad014c023c58c531c92b
Vito Dipinto <vitodipinto@outlook.com>
2019-01-21 20:30:22 +0000

    created category frontpage

--- front-page.php
+++ front-page.php
@@ -14,41 +14,30 @@
 
 get_header();
 ?>
-
-	<div id="primary" class="content-area">
+<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
+		<?php
+		// Check if there are any posts to display
+		if ( have_posts() ) : ?>
+		
+		<div class="work-posts">
+            <?php
+            
+            // The Loop
+            
+            while ( have_posts() ) : the_post();
 
-        <?php
-		if ( have_posts() ) :
-
-
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content-home', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_footer();
+                get_template_part( 'template-parts/content-work', get_post_type() );
+            
+            endwhile; 
+            
+        else: ?>
+            <p>Sorry, no posts matched your criteria.</p>
+            
+            
+        <?php endif; ?>
+	    </div><!-- .work-posts -->
+    </div>
+    
+<?php get_footer(); ?>
