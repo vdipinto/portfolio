91cdb64e05d6dce624217799f94e7a92863a3398
Vito Dipinto <vitodipinto@outlook.com>
2019-04-18 20:00:12 +0100

    addedlinktosingle

--- single.php
+++ single.php
@@ -13,13 +13,23 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
+
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+			?>
+			
+
+			<div class="navigation">
+				<div class="alignleft"><?php previous_post_link( '%link', '%title', true ); ?></div>
+				<div class="alignright"><?php next_post_link( '%link', '%title', true );  ?></div>
+			</div>
+
+			<?php
+			
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
